b9526e97c704ef8fe41fcc4862461335be14def7
Juan Moreira <juanmoreira@alu.ufc.br>
2023-05-24 07:14:56 -0300

    Add files via upload

---
+++ integral.m
@@ -0,0 +1,31 @@
+clear;
+clc;
+close all;
+H = tf(1,[1 0.1 2]);
+[num,den] = tfdata(H,'v');
+[A,B,C,D] = tf2ss(num,den);
+Aa = [A,zeros(size(A,1),size(C,1));-C,zeros(size(C,1),size(C,1))];
+Bu = [B;zeros(size(A,1)+size(C,1)-size(B,1),size(B,2))];
+Ca = [C 0];
+
+[~,n] = size(Aa);
+[~, m] = size(Bu);
+
+Q = sdpvar(n,n,'symmetric');
+N = sdpvar(m,n);
+
+LMI =[Q>=0;
+    Q*Aa'+N'*Bu'+Aa*Q+Bu*N <=0];
+%resolvendo LMI
+options = sdpsettings('solver','sedumi');
+optimize(LMI,[],options)
+Qv = value(Q);
+Nv = value(N);
+%ganho
+Ka =  Nv*inv(Qv);
+
+% Feedback Gain
+K = Ka(1:size(A,1));
+% Integral gain
+H = Ka(size(A,1)+1:end);
+Tsim = 30;
