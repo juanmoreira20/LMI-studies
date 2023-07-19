429b79a6a36906c49703c151246f45f450e3f45c
Juan Moreira <juanmoreira@alu.ufc.br>
2023-07-19 13:04:42 -0300

    H2 Hinf

--- H2-inf/Control/HinfControl.m
+++ H2-inf/Control/HinfControl.m
@@ -1,37 +1,36 @@
 clear all; close all; clc;
-% qsi = 0.1;
-% w = 1;
-% k = 1;
-qsi = 1;
-w = 3;
-k = 20;
 
-A = [-2*qsi*w, -w*w;
-     1,          0];
-B = [1, 0]';
-C = [0, k];
+A = [2, 1, -2;
+     1,-1, -3 ;
+     4, 0, -1];
+B1 = [1, 0;
+    0, 3;
+    3, 1];
+B2 = [1, 0.2, -0.5]';
+C = [2, 1, -0.5];
+D1 = [1, 1];
+D2 = 0.05;
 [aux,n] = size(A);
-[aux,m] = size(B);
+[aux,m] = size(B1);
 [q,aux] = size(C);
-D = 0*sdpvar(q,m);
 gamma = sdpvar(1,1);
-P = sdpvar(n,n);
+X = sdpvar(n,n);
 W = sdpvar(m,n);
 
-a11 = A'*P+P*A;
-a12 = P*B;
-a13 = C';
-a21 = B'*P;
+a11 = (A*X + B1*W)' +A*X + B1*W;
+a12 = B2;
+a13 = (C*X + D1*W)';
+a21 = a12';
 a22 = -gamma*eye(q);
-a23 = D';
-a31 = C;
-a32 = D;
+a23 = D2';
+a31 = a13';
+a32 = a23';
 a33 = -gamma*eye(q);
 
 LMI = [a11, a12, a13;
        a21, a22, a23;
        a31, a32, a33];
-LMI = [LMI <= 0; P>=0];
+LMI = [LMI <= 0; X>=0];
 options = sdpsettings('solver','mosek');
 optimize(LMI,gamma,options)
 C = checkset(LMI)
